8b2bafa65f3e9bd08c6017d8038be25a56eb9a07
ericmault <maulteric@gmail.com>
2021-07-11 10:51:49 -0600

    readability changes

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import tweepy
-import json
-import sqlite3
-import os
+import tweepy, json, sqlite3, os
 from web3 import Web3
 from datetime import datetime
 from config import CONSUMER_KEY, INFURA_URL, ETHERSCAN_TOKEN, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
@@ -10,6 +7,8 @@ from get_data_functions import *
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+# Create API object
+api = tweepy.API(auth)
 
 print(f'Connected to Web3? {w3.isConnected()}')
 print(f'Current eth blocknumber -> {w3.eth.blockNumber}')
@@ -17,30 +16,16 @@ print(f'Current eth blocknumber -> {w3.eth.blockNumber}')
 now = datetime.now()
 today9am = now.replace(hour=9, minute=0, second=0, microsecond=0)
 today905am = now.replace(hour=9, minute=5, second=0, microsecond=0)
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+print("current date and time -> ", dt_string) 
 
 connection = sqlite3.connect(os.path.abspath('fli.db'))
 connection.row_factory = sqlite3.Row
 
 cursor = connection.cursor()
 
-dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-print("current date and time -> ", dt_string) 
-
-# Create API object
-api = tweepy.API(auth)
-
-
-def do_stuff():
-  #api.update_status("----- ETH2x-FLI -----\n Current Leverage Ratio -> "+ getCurrentLeverageRatio(ETHFLI_STRATEGY_ADAPTER_ADDRESS)+"\n Current Supply / Max Supply -> "+ getCurrentAndTotalSupply(ETHFLI_TOKEN_ADDRESS,ETHFLI_SUPPLY_CAP_ISSUANCE_ADDRESS)+ "\n ----- BTC2x-FLI -----\n Current Leverage Ratio -> "+ getCurrentLeverageRatio(BTCFLI_STRATEGY_ADAPTER_ADDRESS)+"\n Current Supply / Max Supply -> "+ getCurrentAndTotalSupply(BTCFLI_TOKEN_ADDRESS,BTCFLI_SUPPLY_CAP_ISSUANCE_ADDRESS)+ "\n")
-# Create a tweet
-  # api.update_status(ETH_Supply())
-  print(ETH_Supply())
-  print(BTC_Supply())
-  #print(str(round(getCurrentSupply(ETHFLI_TOKEN_ADDRESS)/getTotalSupply(ETHFLI_SUPPLY_CAP_ISSUANCE_ADDRESS)*100)))
-  print("done")
-
-
 # class product():
+    # todo create the products as a class and give the parameters as values
 #   pass
 
 def main():
@@ -50,7 +35,6 @@ def main():
     print(BTC_Supply())
   
   # if supply cap is at x% then twitter post
- 
   if round(getCurrentSupply(ETHFLI_TOKEN_ADDRESS)/getTotalSupply(ETHFLI_SUPPLY_CAP_ISSUANCE_ADDRESS)*100) >=90:
     if now.hour % 6 == 0:
       print(ETHsupplyCapWarningThreshold())
@@ -84,7 +68,6 @@ def main():
     cursor.execute(f"UPDATE parameters SET maxSupply = {getTotalSupply(BTCFLI_SUPPLY_CAP_ISSUANCE_ADDRESS)} WHERE product_id = 2")
     connection.commit()
   
-  
   #compare current leverage ratio and if past ripcord threshold then twitter post
   if float(getCurrentLeverageRatio(ETHFLI_STRATEGY_ADAPTER_ADDRESS)) > float(getIncentive(ETHFLI_STRATEGY_ADAPTER_ADDRESS)):
     print(ETHpastRipcordTolerence()) 
@@ -94,7 +77,6 @@ def main():
   
   #Tweet about net asset value disconnect
   
-  
-# do_stuff()
+
 main()
 
